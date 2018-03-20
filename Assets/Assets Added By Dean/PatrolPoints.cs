af277a92281352b311785acf9cd2fa9ddf0980d8
DESKTOP-L97RD5O\deano <dthoma6403@gmail.com>
2018-03-19 21:39:42 -0400

    Continued Terrain Work / Merge
    Pushing and merging all our projects to prepare the test footage for the pitch

--- Assets/Assets Added By Dean/PatrolPoints.cs
+++ Assets/Assets Added By Dean/PatrolPoints.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NPCMove : MonoBehaviour {
+public class PatrolPoints : MonoBehaviour {
 
 	[SerializeField]
 	private Transform destination;
