a14a811674381568b35b113e9b1a958193952b30
Eric Campos <ecamposhome@Erics-MBP.fios-router.home>
2018-03-29 08:29:25 -0400

    ThirdPerson_Test

---
+++ Assets/Eric_Scripts/MC_Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MC_Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W))
+			GetComponent<Animator> ().SetBool ("Running", true);
+		else
+			GetComponent<Animator> ().SetBool ("Running", false);
+		
+	}
+}
