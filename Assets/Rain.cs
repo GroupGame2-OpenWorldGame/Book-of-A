2ad967c6646b15f01b3b70876093ede1cc09d86f
DESKTOP-L97RD5O\deano <dthoma6403@gmail.com>
2018-04-23 12:57:48 -0400

    Rain Script Work and Village Dressing

--- Assets/Rain.cs
+++ Assets/Rain.cs
@@ -5,37 +5,43 @@ using UnityEngine;
 public class Rain : MonoBehaviour 
 {
 
-	//This Script is used to repeatedly set the rain prefab active, wait a bit, deactivate, and so on. With the intention of having organic sporadic
-	//rain coming and going every so often 
-	public GameObject rainPrefab;
+	/*This Script is used to repeatedly set the rain prefab active, wait a bit, deactivate, and so on. With the intention of having organic sporadic
+	*rain coming and going every so often 
 
-	public bool Raining;
+	*/
 
+		float startDelay = 5f;
+		float repeatDelay = 15f;
+
+		public GameObject rainPrefab;
+		public bool continueCoroutine; //bool used to determine if coroutine will continue repeating or not 
+	//	public bool Raining;
+
+	void Start ()
+	{
+		InvokeRepeating ("Wait", startDelay, repeatDelay);
+	}
 
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		/*while (continueCoroutine == true)
+		{
+			yield return StartCoroutine("Wait");
+		}
+		*/
 	}
 
 	IEnumerator Wait()
 	{
-		// suspend execution for 20 seconds
-		yield return new WaitForSeconds(20);
+		// suspend execution for 10 seconds
+		yield return new WaitForSeconds(10);
 		print("Wait" + Time.time);
 		rainPrefab.SetActive (true);
-		// suspend execution for 20 seconds
-		yield return new WaitForSeconds(20);
+		// suspend execution for 10 seconds
+		yield return new WaitForSeconds(10);
 		rainPrefab.SetActive (false);
 
 	}
 
-	IEnumerator Start()
-	{
-		print("Starting " + Time.time);
-
-		// Start function WaitAndPrint as a coroutine
-		yield return StartCoroutine("Wait");
-		print("Done " + Time.time);
-	}
 }
--- Assets/Scenes/BeginGame.cs
+++ Assets/Scenes/BeginGame.cs
@@ -9,26 +9,26 @@ public class BeginGame : MonoBehaviour
 	//This Script will be used on the Start Game Button as a smooth transition into starting the game
 	//The camera will smoothly lerp to the player's position and then be replaced by the player camera
 	//The game will start either by the exposition text arriving on screen before moving to the player, or by moving to the player and then showing the Exposition text
-
-		public Transform startMarker;
-		public Transform endMarker;
+ 		
+		public Transform startMarker;		// lerp start point
+		public Transform endMarker;			// lerp end point
 		public float speed = 1.0F;
-		private float startTime;
+		private float startTime;			
 		private float journeyLength;
 
 	//	public GameObject playerGO;
 	//	public Transform Target; 
 	//	public Camera mainCamera; 
-		public Button startGame; 
+		public Button startGame; 	
 
 	// Use this for initialization
 	void Start ()
 	{
 		
 		Button btn = startGame.GetComponent<Button> ();
-		btn.onClick.AddListener (TaskOnClick);
+		btn.onClick.AddListener (TaskOnClick);		
 
-		startTime = Time.time;
+		startTime = Time.time;		// set the lerp to real time - (frame independent)
 		journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
 	}
 
@@ -44,11 +44,6 @@ public class BeginGame : MonoBehaviour
 
 	}
 		
-
-	void Update() 
-	{
-		
-	}
 
 
 }
