8e383ff38ee0ae589f77f6611fcde168870626cb
JasonRVCC <marcmanzx@yahoo.com>
2018-04-30 22:32:23 -0400

    Working on Ending scene

---
+++ Assets/Scipts/Ending.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Ending : MonoBehaviour {
+
+	[Header("Flags")]
+	[SerializeField]
+	private string flag1;
+	[SerializeField]
+	private string flag2;
+	[SerializeField]
+	private string flag3;
+	[Space(8)]
+
+	[Header("Endings")]
+	[SerializeField]
+	private string ending0;
+	[SerializeField]
+	private string ending1;
+	[SerializeField]
+	private string ending2;
+	[SerializeField]
+	private string ending3;
+	[SerializeField]
+	private string ending1_2;
+	[SerializeField]
+	private string ending1_3;
+	[SerializeField]
+	private string ending2_3;
+	[SerializeField]
+	private string endingAll;
+	[Space(8)]
+
+	[Header("UI")]
+	public Text endingText;
+	public Text flag1Mark;
+	public Text flag2Mark;
+	public Text flag3Mark;
+
+	// Use this for initialization
+	void Start () {
+		SetEnding ();
+	}
+
+	private void SetFlag1Text(bool isTrue){
+		if (isTrue) {
+			flag1Mark.text = "Gained the Sargent's support";
+			flag1Mark.color = Color.green;
+			return;
+		}
+		flag1Mark.text = "Did not gain the Sargent's support";
+		flag1Mark.color = Color.red;
+	}
+
+	private void SetFlag2Text(bool isTrue){
+		if (isTrue) {
+			flag2Mark.text = "Found the mountaintop ambush spot";
+			flag2Mark.color = Color.green;
+			return;
+		}
+		flag2Mark.text = "Did not find an ambush spot";
+		flag2Mark.color = Color.red;
+	}
+
+	private void SetFlag3Text(bool isTrue){
+		if (isTrue) {
+			flag3Mark.text = "Received help from the Wobani";
+			flag3Mark.color = Color.green;
+			return;
+		}
+		flag3Mark.text = "Did not receive outside help";
+		flag3Mark.color = Color.red;
+	}
+
+	public void SetEnding(){
+		if (GameDriver.Instance.IsFlagTrue (flag1)) {
+			SetFlag1Text (true);
+			if (GameDriver.Instance.IsFlagTrue (flag2)) {
+				SetFlag2Text (true);
+				if (GameDriver.Instance.IsFlagTrue (flag3)) {
+					SetFlag3Text (true);
+					endingText.text = endingAll;
+					return;
+				} 
+				SetFlag3Text (false);
+				endingText.text = ending1_2;
+				return;
+			}
+			SetFlag2Text (false);
+			if (GameDriver.Instance.IsFlagTrue (flag3)) {
+				SetFlag3Text (true);
+				endingText.text = ending1_3;
+				return;
+			} 
+			SetFlag3Text (false);
+			endingText.text = ending1;
+			return;
+		}
+		SetFlag1Text (false);
+
+		if (GameDriver.Instance.IsFlagTrue (flag2)) {
+			SetFlag2Text (true);
+			if (GameDriver.Instance.IsFlagTrue (flag3)) {
+				SetFlag3Text (true);
+				endingText.text = ending2_3;
+				return;
+			} 
+			SetFlag3Text (false);
+			endingText.text = ending2;
+			return;
+		}
+		SetFlag2Text (false);
+
+		if (GameDriver.Instance.IsFlagTrue (flag3)) {
+			SetFlag3Text (true);
+			endingText.text = ending3;
+			return;
+		} 
+		SetFlag3Text (false);
+		endingText.text = ending0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scipts/TestEnding.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEnding : MonoBehaviour {
+
+	public string flag1;
+	public string flag2;
+	public string flag3;
+	[Space(8)]
+
+	public bool setFlag1;
+	public bool setFlag2;
+	public bool setFlag3;
+
+	public Ending ending;
+
+	// Use this for initialization
+	void Start () {
+		GameDriver.Instance.SetFlag (flag1, setFlag1.ToString());
+		GameDriver.Instance.SetFlag (flag2, setFlag2.ToString());
+		GameDriver.Instance.SetFlag (flag3, setFlag3.ToString());
+
+		ending.SetEnding ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
